5ecc10abe0487f2cd67ebe5d7737b9c48a09fcac
Thanos Stantzouris <stantz16@hotmail.co.uk>
2018-07-14 20:33:12 +0300

    Made the sidebar a separate file, for less code usage

--- blogList.php
+++ blogList.php
@@ -39,27 +39,7 @@
                 </div>
             </div> <!-- to be repeated -->
         </div>
-        <!-- ###Sidebar Area### -->
-
-        <div class="col-12 col-md-4 col-lg-3">
-            <div class="post-sidebar-area">
-                <!-- Widget Area -->
-                <div class="sidebar-widget-area">
-                    <h5 class="title">Advertisement</h5>
-                </div>
-                <!-- Widget Area -->
-                <div class="sidebar-widget-area">
-                    <h5 class="title">Tags</h5>
-                    <div class="widget-content">
-                        <ul class="tags">
-                            <li><a href="#">Programming</a></li>
-                            <li><a href="#">Hacking</a></li>
-                            <li><a href="#">Arduino</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include "includes/sidebar.php"?>
     </div> <!--End of row-->
 </div> <!--End of container-->
 <?php include "includes/footer.php"?>
---
+++ includes/sidebar.php
@@ -0,0 +1,21 @@
+<!-- ###Sidebar Area### -->
+
+        <div class="col-12 col-md-4 col-lg-3">
+            <div class="post-sidebar-area">
+                <!-- Widget Area -->
+                <div class="sidebar-widget-area">
+                    <h5 class="title">Advertisement</h5>
+                </div>
+                <!-- Widget Area -->
+                <div class="sidebar-widget-area">
+                    <h5 class="title">Tags</h5>
+                    <div class="widget-content">
+                        <ul class="tags">
+                            <li><a href="#">Programming</a></li>
+                            <li><a href="#">Hacking</a></li>
+                            <li><a href="#">Arduino</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
